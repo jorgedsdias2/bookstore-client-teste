2e8bb03d226fe847c632dbad9dc21f13b3bea5c4
Jorge Dias <jorgedsdias@gmail.com>
2019-10-15 21:42:50 -0300

    Subindo ajustes dashboard e varios arquivos

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
-import { Route, Redirect, Link } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 import { isAuthenticated } from '../auth';
+import Sidebar from '../components/Sidebar';
+import Menu from '../components/Menu';
+import Footer from '../components/Footer';
 
 class Default extends Component {
     render() {
         return (
             <>
-                {this.props.children}
-                <Link to="/authors">Authors</Link> / <Link to="/logout">Sign out</Link>
+                <div id="wrapper">
+                    <Sidebar />
+                    <div id="content-wrapper" className="d-flex flex-column">
+                        <div id="content">
+                            <Menu />
+                            {this.props.children}
+                        </div>
+                        <Footer />
+                    </div>
+                </div>
+                <a className="scroll-to-top rounded" href="#page-top">
+                    <i className="fas fa-angle-up"></i>
+                </a>
+
+                <div className="modal fade" id="logoutModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                                <button className="close" type="button" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                            <div className="modal-footer">
+                                <button className="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                                <a className="btn btn-primary" href="/logout">Logout</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </>
         )
     }
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Dashboard() {
     return (
         <>
-            <h1>DASHBOARD</h1>
+            <div className="container-fluid">
+                <h1 className="h3 mb-4 text-gray-800">Dashboard</h1>
+            </div>
         </>
     );
 }
